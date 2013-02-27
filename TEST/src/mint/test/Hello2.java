33ad3acad131a9d6a703b4e39a5f3f64a35f54bf
tata12 <ilakarce@mail.ru>
2013-02-27 09:02:48 +0400

    commit from IDEA

---
+++ TEST/src/mint/test/Hello2.java
@@ -0,0 +1,11 @@
+package mint.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ilya
+ * Date: 2/26/13
+ * Time: 12:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello2 {
+}
